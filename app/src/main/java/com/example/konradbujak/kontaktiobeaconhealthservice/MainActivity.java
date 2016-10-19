ef5e5af3da395a8a64d5ac204571cc6bcc6364f5
konradkontakt <k.bujak@kontakt.io>
2016-10-19 10:37:14 +0200

    Version 1.01 of Beacon Health Service Background Service with dump logs to verbose and info

--- app/src/main/java/com/example/konradbujak/kontaktiobeaconhealthservice/MainActivity.java
+++ app/src/main/java/com/example/konradbujak/kontaktiobeaconhealthservice/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.konradbujak.kontaktiobeaconhealthservice;
 
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+    Intent i = null;
+    private static final String TAG = "MyActivity";
 
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i = new Intent(this, Service.class);
+        Button start = (Button) findViewById(R.id.start_button);
+        Button stop = (Button) findViewById(R.id.stop_button);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(i);
+                if (stopService(i))
+                    Log.i(TAG, "Service stopped" );
+            }
+        });
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(i);
+                if (isMyServiceRunning(Service.class))
+                    Log.i(TAG, "Service started" );
+            }
+        });
+        //For devices with Android v6.0+ we need to ask for permission as it is required by Kontakt.io SDK
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+                Log.i(TAG, "We have already permission" );
+            }
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //startService(new Intent(getBaseContext(), Service.class));
+                    Log.i(TAG, "Permission granted" );
+                } else {
+                    // permission denied, boo!
+                    Log.i(TAG, "Permission denied" );
+                    Context context = getApplicationContext();
+                    CharSequence text = "You have to grant permission in order to use Kontakt.io Beacon Health";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                return;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/konradbujak/kontaktiobeaconhealthservice/Service.java
@@ -0,0 +1,66 @@
+package com.example.konradbujak.kontaktiobeaconhealthservice;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.kontakt.sdk.android.ble.configuration.ScanPeriod;
+import com.kontakt.sdk.android.ble.configuration.scan.ScanMode;
+import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
+import com.kontakt.sdk.android.ble.manager.ProximityManager;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleEddystoneListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
+import com.kontakt.sdk.android.common.KontaktSDK;
+import com.kontakt.sdk.android.common.profile.IBeaconDevice;
+import com.kontakt.sdk.android.common.profile.IBeaconRegion;
+import com.kontakt.sdk.android.common.profile.IEddystoneDevice;
+import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
+
+/**
+ * Created by Konrad.Bujak on 19.10.2016.
+ */
+
+public class Service extends IntentService {
+    private static final String TAG = "MyActivity";
+    public Service() {
+        super("Service");
+    }
+    private ProximityManager KontaktioManager;
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //This is the test API-KEY but to use our sdk properly you have to add here your API KEY
+        KontaktSDK.initialize("QcZNRdfovwLcPVFAvbHgacOnfGBkcHco");
+        if (KontaktSDK.isInitialized())
+            Log.i(TAG, "SDK initialised");
+        KontaktioManager = new ProximityManager(this);
+        KontaktioManager.configuration().monitoringEnabled(true); // enabled by default
+        KontaktioManager.configuration().scanMode(ScanMode.BALANCED);
+        KontaktioManager.configuration().scanPeriod(ScanPeriod.RANGING);
+        //iBeacon scanner ( listener)
+        KontaktioManager.setIBeaconListener(new SimpleIBeaconListener() {
+            @Override
+            public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
+                Log.v(TAG, "IBeacon: " + ibeacon.getUniqueId());
+            }
+        });
+        //Eddystone scanner ( listener)
+        KontaktioManager.setEddystoneListener(new SimpleEddystoneListener() {
+            @Override
+            public void onEddystoneDiscovered(IEddystoneDevice eddystone, IEddystoneNamespace namespace) {
+                Log.v(TAG, "Eddystone: " + eddystone.getUniqueId());            }
+        });
+        //calling the function that start scanning
+        startScanning();
+    }
+    private void startScanning(){
+        KontaktioManager.connect(new OnServiceReadyListener() {
+            @Override
+            public void onServiceReady() {
+                KontaktioManager.startScanning();
+                if (KontaktioManager.isScanning())
+                    Log.i(TAG, "Scan started");
+            }
+        });
+
+    }
+}
