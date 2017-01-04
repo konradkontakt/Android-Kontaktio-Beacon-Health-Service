45359ef0c1f12797ee70b2a77f3a4678f61b1886
konradkontakt <k.bujak@kontakt.io>
2017-01-04 15:56:42 +0100

    Added Beacon Pro listener

--- app/src/main/java/com/example/konradbujak/kontaktiobeaconhealthservice/Service.java
+++ app/src/main/java/com/example/konradbujak/kontaktiobeaconhealthservice/Service.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.kontakt.sdk.android.ble.configuration.ActivityCheckConfiguration;
 import com.kontakt.sdk.android.ble.configuration.ScanPeriod;
 import com.kontakt.sdk.android.ble.configuration.scan.ScanMode;
 import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
@@ -13,11 +14,13 @@ import com.kontakt.sdk.android.ble.manager.listeners.ScanStatusListener;
 import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleEddystoneListener;
 import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
 import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleScanStatusListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleSecureProfileListener;
 import com.kontakt.sdk.android.common.KontaktSDK;
 import com.kontakt.sdk.android.common.profile.IBeaconDevice;
 import com.kontakt.sdk.android.common.profile.IBeaconRegion;
 import com.kontakt.sdk.android.common.profile.IEddystoneDevice;
 import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
+import com.kontakt.sdk.android.common.profile.ISecureProfile;
 
 /**
  * Created by Konrad.Bujak on 19.10.2016.
@@ -25,8 +28,9 @@ import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
 
 public class Service extends IntentService {
     private static final String TAG = "MyActivity";
-    //Replace (Your Secret API key) with your API key aquierd from the Kontakt.io Web Panel
+    //Replace (Your Secret API key) with your API key acquired from the Kontakt.io Web Panel
     public static String API_KEY = "Your Secret API key";
+    public int i;
     public Service() {
         super("Service");
     }
@@ -38,23 +42,35 @@ public class Service extends IntentService {
         if (KontaktSDK.isInitialized())
             Log.i(TAG, "SDK initialised");
         KontaktioManager = new ProximityManager(this);
-        KontaktioManager.configuration().monitoringEnabled(true); // enabled by default
-        KontaktioManager.configuration().scanMode(ScanMode.BALANCED);
-        KontaktioManager.configuration().scanPeriod(ScanPeriod.RANGING);
+        KontaktioManager.configuration().monitoringEnabled(true)
+                .activityCheckConfiguration(ActivityCheckConfiguration.DISABLED)
+                .deviceUpdateCallbackInterval(3000)
+                .scanMode(ScanMode.BALANCED)
+                .scanPeriod(ScanPeriod.RANGING);
         KontaktioManager.setScanStatusListener(createScanStatusListener());
         //iBeacon scanner ( listener)
         KontaktioManager.setIBeaconListener(new SimpleIBeaconListener() {
             @Override
             public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
-                Log.d(TAG, "IBeacon: " + ibeacon.getUniqueId());
+                if(ibeacon.getUniqueId()!=null)
+                    Log.d(TAG, "IBeacon: " + ibeacon.getUniqueId());
             }
         });
         //Eddystone scanner ( listener)
         KontaktioManager.setEddystoneListener(new SimpleEddystoneListener() {
             @Override
             public void onEddystoneDiscovered(IEddystoneDevice eddystone, IEddystoneNamespace namespace) {
-                Log.d(TAG, "Eddystone: " + eddystone.getUniqueId());            }
+                if(eddystone.getUniqueId()!=null)
+                    Log.d(TAG, "Eddystone: " + eddystone.getUniqueId());            }
         });
+        KontaktioManager.setKontaktSecureProfileListener((new SimpleSecureProfileListener(){
+            @Override
+            public void onProfileDiscovered(ISecureProfile profile) {
+                i++;
+                Log.d(TAG, "Beacon Pro: " + profile.getUniqueId());
+
+            }
+        }));
         //calling the function that start scanning
         startScanning();
     }
